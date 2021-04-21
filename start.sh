7b21b058b496908faba226499898961a63c59dc3
Amir Daaee <amir.daaee@gmail.com>
2021-04-21 21:55:59 +0430

    improvement
    added docker-compose and ...

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #/bin/sh
-echo "listen address    " $SNIPROXY_ARG_A
-echo "number of workers   " $SNIPROXY_ARG_W
-if ! [[ -z "${SNIPROXY_ARG_SOCKS}" ]]; then
-  echo "upstram socks5   " $SNIPROXY_ARG_SOCKS
-  ./sniproxy -a $SNIPROXY_ARG_A -w SNIPROXY_ARG_W --socks5 $SNIPROXY_ARG_SOCKS
-else
-  ./sniproxy -a $SNIPROXY_ARG_A -w SNIPROXY_ARG_W
+# todo: better stdout
+echo "listen address    " $BIND_ADDRESS
+echo "number of workers   " $WORKERS_NO
+if ! [[ -z "${USE_TOR}" ]]; then
+  if [[ -z "${UPSTREAM_SOCKS}" ]]; then
+    export UPSTREAM_SOCKS="127.0.0.1:9050"
+  fi
+  tor &
 fi
-
+if ! [[ -z "${UPSTREAM_SOCKS}" ]]; then
+  echo "upstram socks5   " $UPSTREAM_SOCKS
+  ./sniproxy -a $BIND_ADDRESS -w $WORKERS_NO --socks5 $UPSTREAM_SOCKS &
+else
+  ./sniproxy -a $BIND_ADDRESS -w $WORKERS_NO &
+fi
+wait
